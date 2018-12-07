f69a3708ed5c9a114b36f62e1436dfd9a00c0467
Dan E <dan@ehrlichserver.com>
2018-12-07 01:46:01 -0600

    npm and git clone

--- config/linux/ubuntu/ubuntu1804.sh
+++ config/linux/ubuntu/ubuntu1804.sh
@@ -8,12 +8,12 @@ apt-get install apt-transport-https -y
 #aptitude install npm -y
 apt-get install php7.2 npm apache2 php7.2-bcmath build-essential libapache2-mod-evasive php7.2-bz2 php7.2-cgi php7.2-cli php7.2-common php7.2-curl php7.2-dba php7.2-enchant php7.2-fpm php7.2-gd php7.2-gmp php7.2-imap php7.2-interbase php7.2-intl php7.2-json php7.2-ldap php7.2-mbstring php7.2-mysql php7.2-odbc php7.2-opcache php7.2-pgsql php7.2-phpdbg php7.2-pspell php7.2-readline php7.2-recode php7.2-snmp php7.2-soap php7.2-sqlite3 php7.2-tidy php7.2-xml php7.2-xsl php7.2-zip php-redis php-igbinary php7.2-mysql perl tar curl nodejs mysql-client python python-pip imagemagick libapache2-mod-php7.2 git composer vim -y
 
-https://github.com/danehrlich1/openemr.git
+git clone https://github.com/danehrlich1/openemr.git
 rm -rf openemr/.git
 cd openemr 
 composer install 
-nvm install --unsafe-perm 
-nvm run build 
+npm install --unsafe-perm 
+npm run build 
 composer global require phing/phing 
 composer global require phing/phing 
 #/root/.composer/vendor/bin/phing vendor-clean \
@@ -21,7 +21,7 @@ composer global require phing/phing
 composer global remove phing/phing 
 composer dump-autoload -o 
 composer clearcache 
-nvm cache clear --force 
+npm cache clear --force 
 rm -fr node_modules 
 cd ../ 
 chmod 666 openemr/sites/default/sqlconf.php 
