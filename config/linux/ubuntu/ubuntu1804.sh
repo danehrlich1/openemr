3acafcc9a2167472d28e275fb465d6e0f9eba9e9
Dan E <dan@ehrlichserver.com>
2018-12-07 02:08:39 -0600

    apache restart

--- config/linux/ubuntu/ubuntu1804.sh
+++ config/linux/ubuntu/ubuntu1804.sh
@@ -86,3 +86,4 @@ find gacl/admin/templates_c -type d -print0 | xargs -0 chmod 700
 #rm -f ippf_upgrade.php
 #rm -f gacl/setup.php
 #echo "Setup scripts removed, we should be ready to go now!"
+apache2ctl -k restart
