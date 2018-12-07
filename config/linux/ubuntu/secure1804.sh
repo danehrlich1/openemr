7858a0f75d14d0d21a13e9cebe2d0c0b064fbbe7
Dan E <dan@ehrlichserver.com>
2018-12-07 02:18:28 -0600

    beginning of firewall

--- config/linux/ubuntu/secure1804.sh
+++ config/linux/ubuntu/secure1804.sh
@@ -1,1 +1,22 @@
+### ModSecure
 
+apt-get update
+apt-get upgrade -y
+apt-get install libapache2-modsecurity -y
+
+mv /etc/modsecurity/modsecurity.conf-recommended  modsecurity.conf
+
+git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
+cd owasp-modsecurity-crs
+mv crs-setup.conf.example /etc/modsecurity/crs-setup.conf
+mv rules/ /etc/modsecurity/
+
+apache2ctl -k restart
+
+### ModEvasive
+
+### Fail2Ban
+
+### UFW
+
+### SSH Config
