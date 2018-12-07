f677f1ea0d28977d7d35fbd7f2068c8e20ab0373
Dan E <dan@ehrlichserver.com>
2018-12-07 02:36:51 -0600

    v3 modsecurity instead of v2

--- config/linux/ubuntu/secure1804.sh
+++ config/linux/ubuntu/secure1804.sh
@@ -1,15 +1,58 @@
 ### ModSecure
 
-apt-get update
-apt-get upgrade -y
-apt-get install libapache2-modsecurity -y
+apt-get -y update && \
+    apt-get -y install git \
+    libtool \
+    dh-autoreconf \
+    pkgconf \
+    libcurl4-gnutls-dev \
+    libxml2 \
+    libpcre++-dev \
+    libxml2-dev \
+    libgeoip-dev \
+    libyajl-dev \
+    liblmdb-dev \
+    ssdeep \
+    lua5.2-dev \
+    apache2 \
+    apache2-dev
 
-mv /etc/modsecurity/modsecurity.conf-recommended  modsecurity.conf
+cd /opt && \
+    git clone -b v3/master https://github.com/SpiderLabs/ModSecurity
 
-git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
-cd owasp-modsecurity-crs
-mv crs-setup.conf.example /etc/modsecurity/crs-setup.conf
-mv rules/ /etc/modsecurity/
+cd /opt/ModSecurity && \
+    sh build.sh && \
+    git submodule init && \
+    git submodule update && \
+    ./configure && \
+    make && \
+    make install
+
+ln -s /usr/sbin/apache2 /usr/sbin/httpd
+ 
+    
+cd /opt && \
+    git clone https://github.com/SpiderLabs/ModSecurity-apache
+
+cd /opt/ModSecurity-apache/ && \
+    ./autogen.sh && \
+    ./configure && \
+    make && \
+    make install
+    
+mkdir -p /etc/apache2/modsecurity.d/ && \
+    echo "LoadModule security3_module \"$(find /opt/ModSecurity-apache/ -name mod_security3.so)\"" > /etc/apache2/mods-enabled/security.conf && \
+    echo "modsecurity_rules 'SecRuleEngine On'" >> /etc/apache2/mods-enabled/security.conf && \
+    echo "modsecurity_rules_file '/etc/apache2/modsecurity.d/include.conf'" >> /etc/apache2/mods-enabled/security.conf
+    
+
+cd /etc/apache2/modsecurity.d/  && \
+    mv /opt/ModSecurity/modsecurity.conf-recommended /etc/apache2/modsecurity.d/modsecurity.conf && \
+    echo include modsecurity.conf >> /etc/apache2/modsecurity.d/include.conf && \
+    git clone https://github.com/SpiderLabs/owasp-modsecurity-crs owasp-crs && \
+    mv /etc/apache2/modsecurity.d/owasp-crs/crs-setup.conf.example /etc/apache2/modsecurity.d/owasp-crs/crs-setup.conf && \
+    echo include owasp-crs/crs-setup.conf >> /etc/apache2/modsecurity.d/include.conf && \
+    echo include owasp-crs/rules/\*.conf >> /etc/apache2/modsecurity.d/include.conf
 
 apache2ctl -k restart
 
