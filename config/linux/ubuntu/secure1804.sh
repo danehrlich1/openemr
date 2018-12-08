ed7352c789ef68e5c0132cc01a73b261d263cf9f
Dan E <dan@ehrlichserver.com>
2018-12-07 18:42:20 -0600

    update script

--- config/linux/ubuntu/secure1804.sh
+++ config/linux/ubuntu/secure1804.sh
@@ -1,5 +1,8 @@
-### ModSecure
+######
+###### ModSecurity
+######
 
+### Get ModSecurity Prerequisites
 apt-get -y update && \
     apt-get -y install git \
     libtool \
@@ -17,9 +20,9 @@ apt-get -y update && \
     apache2 \
     apache2-dev
 
+### Get Modsecurity V3 and Build
 cd /opt && \
     git clone -b v3/master https://github.com/SpiderLabs/ModSecurity
-
 cd /opt/ModSecurity && \
     sh build.sh && \
     git submodule init && \
@@ -27,25 +30,24 @@ cd /opt/ModSecurity && \
     ./configure && \
     make && \
     make install
-
 ln -s /usr/sbin/apache2 /usr/sbin/httpd
  
-    
+### Get Apache Connector    
 cd /opt && \
     git clone https://github.com/SpiderLabs/ModSecurity-apache
-
 cd /opt/ModSecurity-apache/ && \
     ./autogen.sh && \
     ./configure && \
     make && \
     make install
-    
+
+### Load Module
 mkdir -p /etc/apache2/modsecurity.d/ && \
     echo "LoadModule security3_module \"$(find /opt/ModSecurity-apache/ -name mod_security3.so)\"" > /etc/apache2/mods-enabled/security.conf && \
     echo "modsecurity_rules 'SecRuleEngine On'" >> /etc/apache2/mods-enabled/security.conf && \
     echo "modsecurity_rules_file '/etc/apache2/modsecurity.d/include.conf'" >> /etc/apache2/mods-enabled/security.conf
-    
 
+### Get OWASP Rules
 cd /etc/apache2/modsecurity.d/  && \
     mv /opt/ModSecurity/modsecurity.conf-recommended /etc/apache2/modsecurity.d/modsecurity.conf && \
     echo include modsecurity.conf >> /etc/apache2/modsecurity.d/include.conf && \
@@ -53,20 +55,13 @@ cd /etc/apache2/modsecurity.d/  && \
     mv /etc/apache2/modsecurity.d/owasp-crs/crs-setup.conf.example /etc/apache2/modsecurity.d/owasp-crs/crs-setup.conf && \
     echo include owasp-crs/crs-setup.conf >> /etc/apache2/modsecurity.d/include.conf && \
     echo include owasp-crs/rules/\*.conf >> /etc/apache2/modsecurity.d/include.conf
-
+    cp /opt/ModSecurity/unicode.mapping /etc/apache2/modsecurity.d/
+ 
+### Final Edits
 source /etc/apache2/envvars
+httpd -t
 sed -ie 's/setvar:tx.paranoia_level=1/setvar:tx.paranoia_level=2/g' /etc/apache2/modsecurity.d/owasp-crs/crs-setup.conf
+# remove additional hash signs for paranoia level
 sed -ie 's/SecRuleEngine DetectionOnly/SecRuleEngine On/g' /etc/apache2/modsecurity.d/modsecurity.conf
-apache2ctl -k restart
-
-### ModEvasive
-
-### Fail2Ban
-
-### UFW
-
-sudo ufw allow ‘Apache Full’
-sudo ufw allow ssh
-sudo ufw limit ssh
-
-### SSH Config
+source /etc/apache2/envvars
+apache2ctl -k start
